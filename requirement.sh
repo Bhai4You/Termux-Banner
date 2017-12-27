81c897b31b5a64d6b3ba855e9032fc1447600f32
Sutariya Parixit <32614362+Bhai4You@users.noreply.github.com>
2017-12-27 16:45:16 +0000

    Run First This Script

---
+++ requirement.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+apt update
+apt upgrade
+apt install figlet
+figlet Basic Installation
+apt install toilet
+apt install cowsay
+apt install nano
+apt install ruby
+gem install lolcat
+figlet -f big Done !!! | lolcat
+echo
+echo -e "\e[1m Now Run t-ban.sh...!!!"
+echo
+echo -e "\e[1m\e[32m Developed by :\e[33m Sutariya Parixit (8h4i)"
