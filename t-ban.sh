49d63e7cd7d49b295a6d286796089453e7bf27a6
Sutariya Parixit <32614362+Bhai4You@users.noreply.github.com>
2018-01-04 05:58:01 +0000

    Update t-ban.sh

--- t-ban.sh
+++ t-ban.sh
@@ -24,7 +24,7 @@ echo
 echo -e "\e[1m\e[33m\nWhat is Your Cowsay Name\e[32m :\n\n "
 read varcowsay
 echo  "cowsay -f eyes "$varcowsay" | lolcat" > C0w54y.txt
-echo "toilet -f big '    $varbanner' -F gay | lolcat" > 84nn3r.txt
+echo "toilet -f big ' $varbanner' -F gay | lolcat" > 84nn3r.txt
 echo
 echo "clear" > cl34r.txt
 cat "cl34r.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
