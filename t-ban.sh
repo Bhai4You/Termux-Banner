ec8950f1ac93917872bc4535db9968333410978c
Damaj301damaj-lol <90093996+Damaj301damaj-lol@users.noreply.github.com>
2022-12-04 20:48:00 +0200

    Fix Violation of the first right of libre software
    The right to study the code

--- t-ban.sh
+++ t-ban.sh
@@ -1,3 +1,31 @@
-z="
-";xz='   -';uBz='nner';cz='echo';TCz='es/u';bCz='txt"';Kz='lcat';FCz='ear"';oCz='"84n';TBz=' var';HCz='l34r';HBz='at i';KCz='"cl3';Uz=''\''';Wz=''\''\e[';mCz='hrc';jCz='usr/';fBz='s "$';MCz='xt" ';iCz='les/';az='ow='\''';Oz='6m'\''';BCz=' 84n';JCz='cat ';Vz='red=';JBz='ur \';lz='curi';oz='For ';YBz='y Na';Rz='en='\''';kBz=' > C';FBz='[33m';UCz='sr/e';SCz='/fil';ZBz='\n "';wz='low ';cBz='  "c';GBz='\nWh';Tz=';32m';dBz='y -f';Ez='f bi';lCz='.bas';hBz='y" |';cCz=' >> ';bz=';33m';Yz='m'\''';uz=' " "';gBz='varc';sz='ers.';tBz='arba';lBz='0w54';Hz=' Ban';Iz='ner ';xBz=' | l';pz='Ethi';WCz='ash.';PCz='/dat';Zz='yell';iBz=' lol';YCz='rc';aBz='cows';Sz='\e[1';fz='htgr';Nz='[0;3';RBz='\n"';dz=' -e ';EBz='1m\e';SBz='read';ez='$lig';dCz='a/da';nz='elp ';CCz='n3r.';aCz='54y.';MBz='\e[3';Cz='figl';XCz='bash';DBz=' "';Lz='cyan';LBz='mBan';jBz='cat"';Pz='ligh';qz='cal ';vz='$yel';Gz='rmux';yBz='olca';kz='  Se';BBz=' Par';LCz='4r.t';mz='ty H';ICz='.txt';WBz='ur C';Dz='et -';kCz='etc/';nCz='ls';fCz='om.t';eBz=' eye';sBz=''\'' $v';QCz='a/co';GCz=' > c';Mz='='\''\e';QBz=' :\n';tz='.. "';hz='"\e[';XBz='owsa';CBz='ixit';nBz='t';IBz='s Yo';NBz='3mNa';Fz='g Te';Xz='1;31';qBz=' -f ';KBz='e[31';RCz='m.te';OBz='me\e';Az='clea';pBz='ilet';rz='Hack';ACz='t" >';eCz='ta/c';NCz='>> /';yz='Suta';ABz='riya';mBz='y.tx';gCz='ermu';jz='    ';PBz='[32m';VCz='tc/b';ECz=' "cl';hCz='x/fi';OCz='data';Jz='| lo';ZCz='"C0w';UBz='bann';rBz='big ';bBz='ay';DCz='txt';oBz=' "to';Qz='tgre';VBz='er';wBz=' gay';vBz=''\'' -F';gz='een ';iz='1m  ';Bz='r';
-eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$z$Lz$Mz$Nz$Oz$z$Pz$Qz$Rz$Sz$Tz$Uz$z$Vz$Wz$Xz$Yz$z$Zz$az$Sz$bz$Uz$z$cz$dz$ez$fz$gz$hz$iz$jz$jz$jz$kz$lz$mz$nz$oz$pz$qz$rz$sz$tz$z$cz$uz$z$cz$dz$vz$wz$hz$iz$jz$jz$jz$jz$jz$xz$yz$ABz$BBz$CBz$DBz$z$cz$uz$z$cz$uz$z$cz$uz$z$cz$dz$hz$EBz$FBz$GBz$HBz$IBz$JBz$KBz$LBz$Iz$MBz$NBz$OBz$PBz$QBz$RBz$z$SBz$TBz$UBz$VBz$z$cz$z$cz$dz$hz$EBz$FBz$GBz$HBz$IBz$WBz$XBz$YBz$OBz$PBz$QBz$ZBz$z$SBz$TBz$aBz$bBz$z$cz$cBz$XBz$dBz$eBz$fBz$gBz$XBz$hBz$iBz$jBz$kBz$lBz$mBz$nBz$z$cz$oBz$pBz$qBz$rBz$sBz$tBz$uBz$vBz$wBz$xBz$yBz$ACz$BCz$CCz$DCz$z$cz$z$cz$ECz$FCz$GCz$HCz$ICz$z$JCz$KCz$LCz$MCz$NCz$OCz$PCz$QCz$RCz$Gz$SCz$TCz$UCz$VCz$WCz$XCz$YCz$z$JCz$ZCz$aCz$bCz$cCz$PCz$dCz$eCz$fCz$gCz$hCz$iCz$jCz$kCz$XCz$lCz$mCz$z$nCz$z$JCz$oCz$CCz$bCz$cCz$PCz$dCz$eCz$fCz$gCz$hCz$iCz$jCz$kCz$XCz$lCz$mCz"
+clear
+figlet -f big Termux Banner | lolcat
+cyan='[0;36m'
+lightgreen='[1;32m'
+red='[1;31m'
+yellow='[1;33m'
+echo -e $lightgreen "[1m                Security Help For Ethical Hackers... "
+echo " "
+echo -e $yellow "[1m                         -Sutariya Parixit "
+echo " "
+echo " "
+echo " "
+echo -e "[1m[33m
+What is Your [31mBanner [33mName[32m :
+
+"
+read varbanner
+echo
+echo -e "[1m[33m
+What is Your Cowsay Name[32m :
+
+ "
+read varcowsay
+echo  "cowsay -f eyes "$varcowsay" | lolcat" > C0w54y.txt
+echo "toilet -f big ' $varbanner' -F gay | lolcat" > 84nn3r.txt
+echo
+echo "clear" > cl34r.txt
+cat "cl34r.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "C0w54y.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+ls
+cat "84nn3r.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
